371c1cb222dc6350aeb7b5f95761cf446354a170
Brandon Hill <brandonjon26@gmail.com>
2021-06-11 15:36:10 -0500

    step 1 is'nt done yet

---
+++ Roommates/Repositories/ChoreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    class ChoreRepository : BaseRepository
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = connection)
+
+                conn.Open();
+        }
+    }
+}
